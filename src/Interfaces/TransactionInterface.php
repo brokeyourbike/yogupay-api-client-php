419c52c85c91b435207ee9f80b375ae66f54767e
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-09-03 22:55:25 +0100

    fix: purpose can be null

--- src/Interfaces/TransactionInterface.php
+++ src/Interfaces/TransactionInterface.php
@@ -20,7 +20,7 @@ interface TransactionInterface
     public function getCurrency(): string;
     public function getAmount(): float;
     public function getChannel(): ChannelEnum;
-    public function getPurpose(): string;
+    public function getPurpose(): ?string;
 
     public function getRecipientCountry(): string;
     public function getRecipientName(): string;
